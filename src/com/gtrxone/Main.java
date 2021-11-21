41ec93b92484a8eb9a6d89347f3a432a7319d53b
Goonawardana R N W <gtrxone@gmail.com>
2021-11-21 07:42:01 +0530

    completed and tested

--- src/com/gtrxone/Main.java
+++ src/com/gtrxone/Main.java
@@ -3,6 +3,16 @@ package com.gtrxone;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+        simpleCalculator.setFirstNumber(5.0);
+        simpleCalculator.setSecondNumber(4);
+        System.out.println("add =" + simpleCalculator.getAdditionResult());
+        System.out.println("subtract =" + simpleCalculator.getSubtractionResult());
+        simpleCalculator.setFirstNumber(5.25);
+        simpleCalculator.setSecondNumber(0);
+        System.out.println("multiply =" + simpleCalculator.getMultiplicationResult());
+        System.out.println("divide =" + simpleCalculator.getDivisionResult());
+
     }
 }
